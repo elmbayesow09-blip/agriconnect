e70cfcc7e0f7eb0b343a4c5cd67858adf9461cb5
elmbayesow09-blip <elmbayesow09@gmail.com>
2026-03-19 06:15:01 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// 🔥 CONFIGURATION FIREBASE (à remplacer si besoin)
 const firebaseConfig = {
-
-apiKey: "AIzaSyD7ChJIlfTgvDgzQpyFV_Ol_7a46E2Qfj4",
-
-authDomain: "agriconnect-933f0.firebaseapp.com",
-
-projectId: "agriconnect-933f0",
-
-storageBucket: "agriconnect-933f0.appspot.com",
-
-messagingSenderId: "1028061092951",
-
-appId: "1:1028061092951:web:66afa5dcf60dfeedfad712"
-
+  apiKey: "TA_CLE_API",
+  authDomain: "agriconnect-933f0.firebaseapp.com",
+  projectId: "agriconnect-933f0",
+  storageBucket: "agriconnect-933f0.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
+// 🚀 Initialisation
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
